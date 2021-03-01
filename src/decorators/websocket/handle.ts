7342e6462c495f69cd708102a66d66f1f2be4a52
Jonas Hiptmair <office@hiptmairit.at>
2021-03-01 11:40:47 +0100

    implemented loading management

--- src/decorators/websocket/handle.ts
+++ src/decorators/websocket/handle.ts
@@ -3,11 +3,6 @@ import { WebsocketClient } from "../../client/websocket/client";
 export function Handle(method: string) {
   return function _Handle(target: any, propertyKey: string): any {
     // method annotation
-    const original = target[propertyKey];
-    target[propertyKey] = function handle(...data: any[]) {
-      return original.bind(this)(data);
-    };
     WebsocketClient.registerHandle(method, target, propertyKey);
-    return target;
   };
 }
--- src/decorators/websocket/route.ts
+++ src/decorators/websocket/route.ts
@@ -1,14 +1,18 @@
-export function Route(method: string) {
+export function Route(method: string, loadingKey?: string) {
   return function _Route(target: any, propertyKey: string): any {
     // method annotation
+    if (!target.___loading) target.___loading = {};
+    target.___loading[loadingKey] = 0;
     const original = target[propertyKey];
-    target[propertyKey] = async function execRoute(data: any): Promise<any> {
-      const promise = original.bind(this)();
+    target[propertyKey] = async function execRoute(...data: any): Promise<any> {
+      if (loadingKey) target.___loading[loadingKey]++;
+      const promise = original(...data);
       let res = null;
       if (this.client) {
-        res = await this.client.send(method, data);
+        res = await this.client.send(method, data[0]);
       }
       await promise;
+      if (loadingKey) target.___loading[loadingKey]--;
       return res;
     };
     return target;
